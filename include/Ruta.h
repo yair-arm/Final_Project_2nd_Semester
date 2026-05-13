30fd4f961807bfcdb72f020d7642fb63ccebddc5
Yair <armandoy11007@gmail.com>
2026-05-12 21:27:28 -0500

    Creación de carpetas base para las cabeceras y el código

---
+++ include/Ruta.h
@@ -0,0 +1,13 @@
+//
+// Created by arman on 12/05/2026.
+//
+
+#ifndef PROYECTO_RUTA_H
+#define PROYECTO_RUTA_H
+
+
+class Ruta {
+};
+
+
+#endif //PROYECTO_RUTA_H
---
+++ src/Ruta.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arman on 12/05/2026.
+//
+
+#include "../include/Ruta.h"
