78dccdd5520967079ed9d45ce9181f42d58ced95
Yair <armandoy11007@gmail.com>
2026-05-26 14:35:26 -0500

    Se implementó la cabecera del SistemaTransporte.h, además se establecieron sus métodos que luego serán definidos en el cpp

--- include/SistemaTransporte.h
+++ include/SistemaTransporte.h
@@ -5,17 +5,32 @@
 #ifndef PROYECTO_SISTEMATRANSPORTE_H
 #define PROYECTO_SISTEMATRANSPORTE_H
 #include <vector>
-#include "Ruta.h"
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include "Bus.h"
-#include <nlohmann/json>
+#include "Ruta.h"
+#include "Conductor.h"
+#include "RutaCentro.h"
+#include "RutaBarrio.h"
 
 class SistemaTransporte {
 private:
     std::vector<Ruta*> rutas;
     std::vector<Bus*> buses;
     SistemaTransporte();
+    void cargarDatosDesdeArchivo();
+    void procesarYCrearBuses(nlohmann::json jBuses);
+    void procesarYCrearRutas(const nlohmann::json& jRutas);
+public:
+    static SistemaTransporte& getInstance();
+    ~SistemaTransporte();
+    SistemaTransporte(const SistemaTransporte&) = delete;
+    void operator=(const SistemaTransporte&) = delete;
+    void iniciarSistema();
+    void generarReporteGeneral() const;
+    [[nodiscard]] Bus* consultarBusPorPlaca(const std::string& placa) const;
+    [[nodiscard]] Ruta* consultarRutaPorNombre(const std::string& nombreRuta) const;
 };
-
-
 
 #endif //PROYECTO_SISTEMATRANSPORTE_H
